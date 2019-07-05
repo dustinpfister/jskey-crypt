378c671236d8685b8b0b3b4678a225fcf5ff2c1b
dustinpfister <dustin.pfister@gmail.com>
2019-07-05 15:02:16 -0400

    init command

---
+++ commands/init.js
@@ -0,0 +1,94 @@
+let path = require('path'),
+yaml = require('js-yaml'),
+fs = require('fs'),
+simpleC = require('../lib/simple_crypt');
+
+// make the target folder, and error will occur if the folder is there
+let makeFolder = (dir_project) => {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(dir_project, function (e) {
+            if (e) {
+                reject(e);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+// make key.yaml
+let makeKeyFile = (dir_project, key) => {
+    return new Promise((resolve, reject) => {
+        let data = yaml.safeDump({
+                key: key
+            });
+        fs.writeFile(path.join(dir_project, 'key.yaml'), data, (e) => {
+            if (e) {
+                reject(e);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+// make first post
+let makeFirstPost = function (dir, opt_crypt) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('../README.md', 'utf8', (e, text) => {
+            if (e) {
+                text = 'first post file not found';
+            }
+            fs.writeFile(path.join(dir, '0.md'), simpleC.toHex(text, opt_crypt), (e) => {
+                if (e) {
+                    reject(e);
+                } else {
+                    resolve();
+                }
+            });
+        })
+    });
+};
+
+// define command
+exports.command = 'init';
+exports.aliases = ['i'];
+exports.describe = 'init a new project folder';
+exports.builder = {
+    // target folder
+    t: {
+    default:
+        'blog_posts'
+    },
+    // key
+    k: {
+    default:
+        'spaceballs'
+    }
+};
+exports.handler = function (argv) {
+
+    let dir_target = path.join(process.cwd(), argv.t),
+    dir_posts_crypt = path.join(dir_target, '_posts_crypt');
+
+    makeFolder(dir_target).then(() => {
+        console.log('target folder ' + argv.t + ' created at: ');
+        console.log(dir_target);
+        return makeKeyFile(dir_target, argv.k);
+    }).then(() => {
+        return makeFolder(dir_posts_crypt);
+    }).then(() => {
+        return makeFirstPost(dir_posts_crypt, {
+            password: argv.k
+        });
+    }).then(() => {
+
+        console.log('setup compleate!');
+
+    }).catch ((e) => {
+
+        console.log(e.message);
+
+    });
+
+};
--- roadmap.md
+++ roadmap.md
@@ -1,5 +1,11 @@
-## 0.0.x - Alpha I
+## 1.0.x - Server command feature
+  * server command started
+  * server provides a /read?p=postname path that responds to get requests
+  * server /read path also responds to post requests
+  * server provides a /write path that responds to post requests only
+
+## 0.0.x - Alpha I - first release
   * (done) Uses yargs and a default command fires when called with no arguments
-  * can be installed globally as jskey-crypt
-  * can create a new empty /blog_posts folder with a keys.yaml file and a /blog_posts/_posts_crypt folder
+  * (done) can be installed globally as jskey-crypt
+  * can create a new empty /blog_posts folder with a keys.yaml file and a /blog_posts/_posts_crypt folder (init command)
   * can create an encrypted file in a _posts_crypt folder
